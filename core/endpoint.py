9bf6dfea665d8a4b20f3a84f8ad35ee2d0c7e64d
Enrico <enrico.fusto@protonmail.com>
2026-04-15 10:55:24 +0200

    feat: Added endpoint and validation modules

---
+++ core/endpoint.py
@@ -0,0 +1,49 @@
+import requests
+from typing import Optional, Any
+from core.parser import AmapListParser
+
+class AmapList:
+    BASE_URI: str = "https://amap-idf.org"
+    AMAP_LIST_PATH: str = "les-amap/trouver-une-amap-en-idf"
+    HEADERS: dict[str, str] = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "fr-FR,fr;q=0.9,en;q=0.8",
+        "Referer": f"{BASE_URI}/{AMAP_LIST_PATH}",
+    }
+
+    def __init__(self) -> None:
+        self.__uri: str = f"{self.BASE_URI}/{self.AMAP_LIST_PATH}"
+        self.__session: Optional[requests.Session] = None
+
+    def __ensure_session(self, force: bool = False) -> requests.Session:
+        if force or not self.__session:
+            session = requests.Session()
+            session.headers.update(self.HEADERS)
+            session.get(self.__uri)
+            self.__session = session
+        return self.__session
+
+    def call(self, data: dict[str, str]) -> list[dict[str, Any]]:
+        session = self.__ensure_session()
+        try:
+            ret = session.post(self.__uri, data=data)
+            ret.raise_for_status()
+        except requests.RequestException:
+            session = self.__ensure_session(force=True)
+            ret = session.post(self.__uri, data=data)
+            ret.raise_for_status()
+
+        return AmapListParser().parse(ret.text)
+
+
+class ZipCodeInfo:
+    BASE_URI: str = "https://geo.api.gouv.fr/communes"
+
+    def call(self, zip_code: str) -> dict[str, Any]:
+        try:
+            ret = requests.get(f"{self.BASE_URI}/{zip_code}/?fields=centre&format=json")
+            ret.raise_for_status()
+            return ret.json()
+        except requests.RequestException:
+            return {}
---
+++ core/validations.py
@@ -0,0 +1,43 @@
+from core.endpoint import ZipCodeInfo
+
+ALLOWED_DEPTS: list[str] = ["75", "77", "78", "91", "92", "93", "94", "95"]
+ALLOWED_RADII: list[str] = ["2", "5", "10", "15", "20"]
+
+DEFAULT_DEPT: str = "75"
+DEFAULT_RADIUS: str = "2"
+
+class ValidationError(RuntimeError):
+    pass
+
+def validate_department(department: int|str) -> str:
+    dept = str(department)
+    if dept not in ALLOWED_DEPTS:
+        raise ValidationError(f"Parameter `department` must belong to {ALLOWED_DEPTS}, but {dept} was given")
+    return dept
+    
+def validate_km_radius(km_radius: int|str) -> str:
+    km_r = str(km_radius)
+    if km_r not in ALLOWED_RADII:
+        raise ValidationError(f"Parameter `km_radius` must belong to {ALLOWED_RADII}, but {km_r} was given")
+    return km_r
+
+def validate_zip_code(zip_code: int|str) -> str:
+    try:
+        zc = str(int(zip_code))
+        zc_info = ZipCodeInfo().call(zc)
+        zc = zc_info.get("code")
+        if not zc:
+            raise ValueError
+        return zc
+    except ValueError:
+        raise ValidationError(f"Parameter `zip_code` must be represented by a valid french zip code number, but {zip_code} was given")
+
+def validate_coordinates(
+    latitude: float|str,
+    longitude: float|str
+) -> tuple[str, str]:
+    try:
+        return (f"{float(latitude):.4f}", f"{float(longitude):.4f}")
+    except ValueError:
+        raise ValidationError(f"Parameters `latitude` and `longitude` must be represent floating point numbers, but ({longitude}, {latitude}) were given")
+
